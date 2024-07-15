a281303efa8dcbb82d34f7504e3bd411dfc5158b
Daniel Kukula <daniel.kuku@gmail.com>
2024-07-15 23:46:43 +0100

    publish 0.1.0

--- mix.exs
+++ mix.exs
@@ -5,9 +5,33 @@ defmodule AddressFormatting.MixProject do
     [
       app: :address_formatting,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      description: description()
+    ]
+  end
+
+  defp description do
+    "address formatting implemetation"
+  end
+
+  defp package do
+    [
+      files: [
+        "lib",
+        "mix.exs",
+        "README*",
+        "address-formatting/conf/countries/*",
+        "address-formatting/conf/abbreviations/*",
+        "address-formatting/conf/*"
+      ],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/dkuku/ex_address_formatting",
+        "Original project homepage" => "https://github.com/OpenCageData/address-formatting/"
+      }
     ]
   end
 
@@ -23,7 +47,8 @@ defmodule AddressFormatting.MixProject do
     [
       {:bbmustache, "~> 1.12"},
       {:yaml_elixir, "~> 2.7"},
-      {:benchee, "~> 1.0", only: :dev}
+      {:benchee, "~> 1.0", only: :dev},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 end
